30769d3991126ff2a8c31e1a064003f389cb0cf4
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2022-02-23 15:52:10 +0100

    created app launcher

--- src/main/java/airlineManager/AirlineManagerApp.java
+++ src/main/java/airlineManager/AirlineManagerApp.java
@@ -1,5 +1,21 @@
 package airlineManager;
 
-public class AirlineManagerApp {
-    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AirlineManagerApp extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Airline Manager");
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("App.fxml"))));
+        primaryStage.show();
+    }
 }
