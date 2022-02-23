82b626994db1721b556929ac2eeaf448e35307cd
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2022-02-23 16:05:36 +0100

    getters and setters for aircraft

--- src/main/java/airlineManager/Aircraft.java
+++ src/main/java/airlineManager/Aircraft.java
@@ -6,8 +6,57 @@ public class Aircraft {
     private String model;
     private String type; // Passanger or Cargo
     private int speed;
-    private int price;
     private int efficiency;
+    private int price;
+
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getEfficiency() {
+        return efficiency;
+    }
+    
+    public void setEfficiency(int efficiency) {
+        this.efficiency = efficiency;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
     
 
 }
