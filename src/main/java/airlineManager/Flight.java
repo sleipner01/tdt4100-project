1817a502d33e585c440bc857f0abad04328dab0d
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2022-02-28 18:05:52 +0100

    moved passengers to plane, instead of flight

--- src/main/java/airlineManager/Flight.java
+++ src/main/java/airlineManager/Flight.java
@@ -1,33 +1,19 @@
 package airlineManager;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Flight {
 
     private Plane plane;
-    private List<Passenger> passengers;
     private Airport origin;
     private Airport destination;
 
 
-    public Flight(Plane plane, List<Passenger> passengers, Airport origin) {
+    public Flight(Plane plane, Airport origin) {
         this.plane = plane;
-        this.passengers = new ArrayList<>();
         this.origin = origin;
     }
 
     public Plane getPlane() {
         return this.plane;
-    }
-
-    public List<Passenger> getPassengers() {
-        return this.passengers;
-    }
-
-
-    public void addPassenger(Passenger passenger) {
-        this.passengers.add(passenger);
     }
 
     public Airport getOrigin() {
--- src/main/java/airlineManager/Plane.java
+++ src/main/java/airlineManager/Plane.java
@@ -1,15 +1,21 @@
 package airlineManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Plane {
 
     private Aircraft aircraft;
     private String nickName;
     private Airline airline;
+    private List<Passenger> passengers;
+
 
     public Plane(Aircraft aircraft, String nickName, Airline airline) {
         this.aircraft = aircraft;
         this.nickName = nickName;
         this.airline = airline;
+        this.passengers = new ArrayList<>();
     }
 
     public Aircraft getAircraft() {
@@ -26,6 +32,18 @@ public class Plane {
 
     public Airline getAirline() {
         return airline;
+    }
+
+    public List<Passenger> getPassengers() {
+        return this.passengers;
+    }
+
+    public void addPassenger(Passenger passenger) {
+        this.passengers.add(passenger);
+    }
+
+    public void clearPassengers() {
+        this.passengers.removeAll(this.passengers);
     }
     
 }
