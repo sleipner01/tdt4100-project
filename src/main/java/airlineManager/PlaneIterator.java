4a9d0682f242e8ceaa8374e2f1eafe14b6fea2f3
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2022-03-11 09:34:07 +0100

    added planeIterator so I can easily iterate through the airlines planes

---
+++ src/main/java/airlineManager/PlaneIterator.java
@@ -0,0 +1,31 @@
+package airlineManager;
+
+import java.nio.channels.IllegalSelectorException;
+import java.util.Iterator;
+import java.util.List;
+
+public class PlaneIterator implements Iterator<Plane> {
+
+    private int index;
+    private int listLength;
+    private List<Plane> planes;
+
+    public PlaneIterator(List<Plane> planes) {
+        this.planes = planes;
+        this.listLength = planes.size();
+    }
+
+    @Override
+    public boolean hasNext() {
+        if(listLength - index > 0) return true;
+        return false;
+    }
+
+    @Override
+    public Plane next() {
+        if(index == listLength || index > listLength)
+            throw new IllegalArgumentException("There are no more planes in the airline.");
+        return planes.get(index++);
+    }
+    
+}
