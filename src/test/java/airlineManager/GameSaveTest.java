0dd8f7288f40b00dfbe2d22336e119721310dfa2
Magnus Byrkjeland <=>
2022-04-29 14:34:29 +0200

    gamesaveload test

--- src/test/java/airlineManager/GameSaveTest.java
+++ src/test/java/airlineManager/GameSaveTest.java
@@ -1,48 +1,103 @@
 package airlineManager;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.junit.jupiter.api.Test;
+
 public class GameSaveTest {
 
     private AirlineManagerGame createGame() {
+        AirlineManagerGame game = new AirlineManagerGame();
+        game.airlineBuy(game.getAircrafts().get(0));
+        game.boardPassenger(game.getAirline().getPlanes().get(0),
+                            game.getAirline().getPlanes().get(0).getAirport().getTravellers().get(0));
 
-	}
-	
-	private String getStringRep() {
-		return "# name\nTestList\n# entries\nE1\nE2\nE3\nE4\n";
+        return game;
 	}
 	
 	@Test
-	public void testWriteToOS() {
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		TodoList tl = getTodoList();
-		TodoFileSupport fs = new TodoFileSupport();
-		fs.writeTodoList(tl, os);
+	public void testSaveGame() {
+		AirlineManagerGame game = this.createGame();
+		InterfaceGameSaveHandler gameSaveHandler = new GameSaveHandler();
+		gameSaveHandler.save("testSave", game);
+
+        Airline airline = game.getAirline();
+        Plane plane = game.getAirline().getPlanes().get(0);
+        Passenger passenger = plane.getPassengers().get(0); 
+
+        String content = null;
 		
-		String actual = new String(os.toByteArray());
-		String expected = getStringRep();
+        try {
+            content = Files.readString(new File(getClass().getResource("gamefiles/").getFile() + "testSave.txt").toPath());
+        }
+        catch (IOException e) {
+            fail("Error when reading file");
+            e.printStackTrace();
+        }
+
+		String actual = content;
+		String expected = "***VALID\n" +
+                          "\n\n\n" +
+                          "***AIRLINE\n" + 
+                          "Airline," + 
+                          airline.getName() + "," + 
+                          airline.getCoinAmount() + "," + 
+                          airline.getHomeAirport().getAirportID() + "\n" +
+                          "\n\n\n" +
+                          "***PLANES\n" +
+                          "Plane," + 
+                          plane.getNickName() + "," +
+                          plane.getAircraft().getAircraftID() + "," +
+                          plane.getAirport().getAirportID() + "," +
+                          "null," +
+                          plane.isInFlight() + "," +
+                          plane.getRemainingFlightTimeInMinutes() + "\n" + 
+                          "Passenger," + 
+                          passenger.getFullName() + "," +
+                          passenger.getDestination().getAirportID() + "\n" +
+                          "\n\n";
+
 		
-		assertEquals(expected, actual, "Written string representation is not correct.");
+		assertEquals(expected, actual, "Information written to the file is in the wrong format.");
 	}
 	
 	@Test
-	public void testReadFromIS() throws UnsupportedEncodingException {
-		InputStream is = new ByteArrayInputStream(getStringRep().getBytes("UTF-8"));
+	public void testLoadGame() {
+		AirlineManagerGame game = this.createGame();
+		InterfaceGameSaveHandler gameSaveHandler = new GameSaveHandler();
+		gameSaveHandler.save("testLoad", game);
 
-		TodoFileSupport fs = new TodoFileSupport();
-		TodoList actual = fs.readTodoList(is);
-		TodoList expected = getTodoList();
-		
-		var actualIterator = actual.iterator();
-		var expectedIterator = expected.iterator();
-		
-		int i = 0;
-		while (expectedIterator.hasNext()) {
-			try {
-				assertEquals(expectedIterator.next().getText(), actualIterator.next().getText(), "Element mismatch at position " + i);
-			} catch (IndexOutOfBoundsException e) {
-				fail("The read list does not contain the correct number of elements.");
-			}
-			i++;
-		}
-		assertEquals(expected.getName(), actual.getName(), "Names does not match.");
+        Airline expectedAirline = game.getAirline();
+        Plane expectedPlane = game.getAirline().getPlanes().get(0);
+        Passenger expectedPassenger = expectedPlane.getPassengers().get(0); 
+
+        Airline actualAirline = null;
+        
+        try {
+            actualAirline = gameSaveHandler.load(new File(getClass().getResource("gamefiles/").getFile() + "testLoad.txt"), game);
+        }
+        catch (FileNotFoundException e) {
+            fail("The test couldn't find the file");
+        }
+        catch (IllegalArgumentException e) {
+            fail("The test couldn't find the file");
+        }
+        Plane actualPlane = actualAirline.getPlanes().get(0);
+        Passenger actualPassenger = actualPlane.getPassengers().get(0); 
+
+		assertEquals(expectedAirline.getCoinAmount(), actualAirline.getCoinAmount(), "The coinamounts are different.");
+		assertEquals(expectedAirline.getHomeAirport().getAirportID(), actualAirline.getHomeAirport().getAirportID(), "The home airports are different.");
+		assertEquals(expectedPlane.getAircraft().getAircraftID(), actualPlane.getAircraft().getAircraftID(), "The aircrafts are different.");
+		assertEquals(expectedPlane.getAirport().getAirportID(), actualPlane.getAirport().getAirportID(), "The airports are different.");
+		assertEquals(expectedPassenger.getFullName(), actualPassenger.getFullName(), "The passengers name are different.");
+		assertEquals(expectedPassenger.getPaying(), actualPassenger.getPaying(), "The passengers are paying different amounts.");
+		assertEquals(expectedPassenger.getDestination(), actualPassenger.getDestination(), "The passengers are going different places.");
+
 	}
 }
